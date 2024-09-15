cfa59d477d92d80d6ca25d8d12e5a569900b2c11
Orn Bona <ornbona861@gmail.com>
2024-09-15 21:29:53 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="w-screen  mt-12 flex items-center justify-center">
+      <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">Portfolio Coming Soon...</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import commonjs from "@rollup/plugin-commonjs";
+import image from "@rollup/plugin-image";
+import resolve, { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default (({mode}: { mode: string }) => {
+	return defineConfig({
+		// Development server configuration
+		server: {
+			port: 3000, // Specify the development server port
+			open: true, // Automatically open the browser
+		},
+		// Build configuration
+		build: {
+			rollupOptions: {
+				// input: 'src/main.tsx',
+				// Apply tree shaking to remove unused code
+				treeshake: true,
+				// Use the terser plugin to minify your JavaScript
+				plugins: [
+					// Add the Terser plugin for minification
+					terser({
+						compress: true,
+						ecma: 2015,
+						safari10: true,
+						sourceMap: true,
+					}),
+					resolve(),
+					commonjs(),
+					image(),
+					nodeResolve(),
+				],
+				// Output configuration
+				output: {
+					dir: "dist",
+					chunkFileNames: "chunks/[hash].js",
+					assetFileNames: "assets/[hash][extname]",
+					format: "es",
+					// Set the entry file name
+					entryFileNames: "[hash].js",
+				},
+			},
+			sourcemap: "hidden",
+			minify: mode === "production", // Minify the code in production mode
+			chunkSizeWarningLimit: 500, // Adjust the chunk size warning limit as needed
+			cssMinify: mode === "production", // Enable CSS minification in production mode
+			dynamicImportVarsOptions: {
+				include: ["*.css", "*.less", "*.sass", "*.scss"],
+				exclude: ["node_modules"],
+			},
+			manifest: true, // Generate manifest.json file
+		},
+		plugins: [react()],
+	});
+});
